ab243b4a5f3a7f47c3e13f8a1c42b0d0b30bf1b7
abeauquel <beauquel.alexandre@gmail.com>
2018-11-03 11:40:38 -0400

    Feat : Creation du DAO Humidite

---
+++ src/dao/HumiditeDAO.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xela
+ * Date: 11/3/18
+ * Time: 11:22 AM
+ */
+
+require 'BaseDeDonnees.php';
+require 'modele/Humidite.php';
+require 'modele/pluriel/Humidites.php';
+
+abstract class HumiditeDAO
+{
+    const SQL_LISTE_HUMIDITES = "SELECT * FROM humidite";
+
+    public static function listerHumidite(){
+
+        $requeteListeHumidites = BaseDeDonnees::getInstance()->prepare(self::SQL_LISTE_HUMIDITES);
+        $requeteListeHumidites->execute();
+        $curseur = $requeteListeHumidites->fetchAll();
+
+        $listeHumidites = new Humidites([]);
+        foreach ($curseur as $ligne){
+            $humidite = new Humidite($ligne['id'], $ligne['valeur'], $ligne['date']);
+            $listeHumidites->ajouter($humidite);
+        }
+        return $listeHumidites;
+    }
+
+}
