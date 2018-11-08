f0905486283a11903b98b821ecc57ec267d860e4
abeauquel <beauquel.alexandre@gmail.com>
2018-11-08 11:22:35 -0500

    Feat : Ajout d'un order by

--- src/dao/HumiditeSQL.php
+++ src/dao/HumiditeSQL.php
@@ -8,8 +8,8 @@
 
 abstract class HumiditeSQL
 {
- protected static $SQL_LISTE_HUMIDITES = "SELECT * FROM humidite";
- protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_DEBUT = "SELECT * FROM humidite  WHERE date > :dateDebut";
- protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_FIN = "SELECT * FROM humidite WHERE date < :dateFin";
- protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_DEBUT_ET_FIN = "SELECT * FROM humidite where date between :dateDebut and :dateFin";
+ protected static $SQL_LISTE_HUMIDITES = "SELECT * FROM humidite order by date DESC ";
+ protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_DEBUT = "SELECT * FROM humidite  WHERE date > :dateDebut order by date DESC ";
+ protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_FIN = "SELECT * FROM humidite WHERE date < :dateFin order by date DESC ";
+ protected static $SQL_LISTE_HUMIDITES_AVEC_DATE_DEBUT_ET_FIN = "SELECT * FROM humidite where date between :dateDebut and :dateFin order by date DESC ";
 }
